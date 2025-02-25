67bbeaa9cb404b0a2a85e3fd8e4df68908a15f64
“Edcodigo01” <edcodigo01@gmail.com>
2025-02-25 10:46:53 -0500

    test

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const app = express();
 const port = 3000;
 
 // Middleware para capturar el subdominio
-app.use((req, res, next) => {
-    const host = req.headers.host;
-    const subdomain = host.split('.')[0]; // Extraer el subdominio
-    req.subdomain = subdomain;
-    next();
-});
+// app.use((req, res, next) => {
+//     const host = req.headers.host;
+//     const subdomain = host.split('.')[0]; // Extraer el subdominio
+//     req.subdomain = subdomain;
+//     next();
+// });
 
 // Ruta GET
 app.get('/', (req, res) => {
-    res.json({ message: `El subdominio es: "${req.subdomain}"` });
+    res.json({ message: `prueba` });
+
+    // res.json({ message: `El subdominio es: "${req.subdomain}"` });
 });
 
 // Iniciar servidor
